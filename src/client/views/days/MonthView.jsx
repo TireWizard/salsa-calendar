9d75fc4fadd31072b41d5aa515b4c3bad5d80951
derekg23 <dgalbraith523@gmail.com>
2015-11-24 16:14:06 -0500

    Update MonthView.jsx

--- src/client/views/days/MonthView.jsx
+++ src/client/views/days/MonthView.jsx
@@ -18,6 +18,15 @@ export default class DaysWrapper extends PureView {
 
   render() {
     const { daysInWeek, days, startOfWeek, firstDay, onDaySelected, classDecorator } = this.props;
+    
+    console.log(
+        'daysInWeek '+daysInWeek+"\n"+
+        'days '+days+"\n"+
+        'startOfWeek '+startOfWeek+"\n"+
+        'firstDay '+firstDay+"\n"+
+        'onDaySelected '+onDaySelected+"\n"+
+        'classDecorator '+classDecorator
+      );
 
     return (
       <div className="month-view" data-week-starts={startOfWeek} data-first-day={firstDay}>
